d95eea1e12dc0169eb529cf37bae80b75fda2479
devjson <dev.json.stringify@gmail.com>
2021-05-23 07:34:31 +0800

    Update: adjust footer position

--- components/Layout/Footer/Footer.js
+++ components/Layout/Footer/Footer.js
@@ -4,9 +4,8 @@ import Box from '@material-ui/core/Box'
 const StyledBoxFooter = styled(Box)`
   bottom: 0;
   background-color: #ffc300;
-  position: absolute;
-  left: 0;
-  right: 0;
+  position: relative;
+  margin-top: 1rem;
   min-height: 5rem;
 `
 const Footer = () => {
