20a07830272377bb376e67a879fb1bb8a04d3a4e
devjson <dev.json.stringify@gmail.com>
2021-05-23 22:31:53 +0800

    Add: animation on form component

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -4,10 +4,12 @@ import { Form, Field } from 'react-final-form'
 import Box from '@material-ui/core/Box'
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
+import Grow from '@material-ui/core/Grow'
 import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 import Paper from '@material-ui/core/Paper'
 import styled from 'styled-components'
+import HomeContext from 'views/HomeView/HomeContext'
 
 const StyledContainer = styled(Container)`
   display: flex;
@@ -20,86 +22,89 @@ const onSubmit = async (values) => {
 }
 
 const FinalForm = () => {
+  const { isFormVisible } = useContext(HomeContext)
   return (
-    <StyledContainer maxWidth="md">
-      <Box margin="5rem auto" height="100%">
-        <Paper>
-          <Box padding="5rem" boxSizing="border-box">
-            <Box marginBottom="2rem">
-              <Typography variant="h4">
-                Find Youtube videos by channel name
-              </Typography>
-            </Box>
-            <Form
-              onSubmit={onSubmit}
-              render={({
-                handleSubmit,
-                form,
-                submitting,
-                pristine,
-                values,
-              }) => (
-                <form onSubmit={handleSubmit}>
-                  <Box
-                    display="grid"
-                    gridTemplateColumns="1fr 1fr"
-                    gridTemplateRows="1fr"
-                    gap="1rem 1rem"
-                    margin="2rem auto"
-                  >
-                    <Box display="flex">
-                      <Box>
-                        <Typography>Hi, my name is</Typography>
-                      </Box>
-                      <Field name="name">
-                        {(props) => (
-                          <Box margin="0 1rem" flexGrow="3">
-                            <TextField
-                              fullWidth
-                              name={props.input.name}
-                              value={props.input.value}
-                              onChange={props.input.onChange}
-                            />
-                          </Box>
-                        )}
-                      </Field>
-                    </Box>
-                    <Box display="flex">
-                      <Box>
-                        <Typography>and I'm looking for</Typography>
-                      </Box>
-                      <Field name="youtubeUser">
-                        {(props) => (
-                          <Box margin="0 1rem" flexGrow="3">
-                            <TextField
-                              fullWidth
-                              name={props.input.name}
-                              value={props.input.value}
-                              onChange={props.input.onChange}
-                            />
-                          </Box>
-                        )}
-                      </Field>
-                    </Box>
-                  </Box>
-
-                  <Box>
-                    <Button
-                      type="submit"
-                      disabled={submitting || pristine}
-                      color="secondary"
-                      variant="contained"
+    <Grow in={isFormVisible}>
+      <StyledContainer maxWidth="md">
+        <Box margin="5rem auto" height="100%">
+          <Paper>
+            <Box padding="5rem" boxSizing="border-box">
+              <Box marginBottom="2rem">
+                <Typography variant="h4">
+                  Find Youtube videos by channel name
+                </Typography>
+              </Box>
+              <Form
+                onSubmit={onSubmit}
+                render={({
+                  handleSubmit,
+                  form,
+                  submitting,
+                  pristine,
+                  values,
+                }) => (
+                  <form onSubmit={handleSubmit}>
+                    <Box
+                      display="grid"
+                      gridTemplateColumns="1fr 1fr"
+                      gridTemplateRows="1fr"
+                      gap="1rem 1rem"
+                      margin="2rem auto"
                     >
-                      Find now
-                    </Button>
-                  </Box>
-                </form>
-              )}
-            />
-          </Box>
-        </Paper>
-      </Box>
-    </StyledContainer>
+                      <Box display="flex">
+                        <Box>
+                          <Typography>Hi, my name is</Typography>
+                        </Box>
+                        <Field name="name">
+                          {(props) => (
+                            <Box margin="0 1rem" flexGrow="3">
+                              <TextField
+                                fullWidth
+                                name={props.input.name}
+                                value={props.input.value}
+                                onChange={props.input.onChange}
+                              />
+                            </Box>
+                          )}
+                        </Field>
+                      </Box>
+                      <Box display="flex">
+                        <Box>
+                          <Typography>and I'm looking for</Typography>
+                        </Box>
+                        <Field name="youtubeUser">
+                          {(props) => (
+                            <Box margin="0 1rem" flexGrow="3">
+                              <TextField
+                                fullWidth
+                                name={props.input.name}
+                                value={props.input.value}
+                                onChange={props.input.onChange}
+                              />
+                            </Box>
+                          )}
+                        </Field>
+                      </Box>
+                    </Box>
+
+                    <Box>
+                      <Button
+                        type="submit"
+                        disabled={submitting || pristine}
+                        color="secondary"
+                        variant="contained"
+                      >
+                        Find now
+                      </Button>
+                    </Box>
+                  </form>
+                )}
+              />
+            </Box>
+          </Paper>
+        </Box>
+      </StyledContainer>
+    </Grow>
   )
 }
 
