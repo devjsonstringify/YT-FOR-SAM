186c9e46db75f04f8c4e7f056017d44173f5c8f0
devjson <dev.json.stringify@gmail.com>
2021-06-02 16:39:32 +0800

    Update:  copy from https://github.com/mui-org/material-ui/blob/master/examples/nextjs/pages/_document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheets } from '@material-ui/core/styles'
-import theme from '../theme'
+import theme from 'theme'
 
 export default class MyDocument extends Document {
   render() {
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Concert+One&family=Fredoka+One&display=swap"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
         </Head>
         <body>
